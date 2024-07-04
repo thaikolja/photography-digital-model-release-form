8958fb2424dfead8899770b491dcfd6724a916fe
Kolja Nolte <kolja.nolte@gmail.com>
2024-07-05 05:31:38 +0700

    First commit with basic file templates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
   compatibilityDate: "2024-07-05",
-  modules: ["@nuxt/fonts", "@nuxtjs/tailwindcss", "@vueuse/nuxt", "@nuxtjs/i18n"]
+  modules: [
+  	"@nuxt/fonts",
+	"@nuxtjs/tailwindcss",
+	"@vueuse/nuxt",
+	"@nuxtjs/i18n"
+  ],
+  runtimeConfig: {
+      apiKey: '' // Default to an empty string, automatically set at runtime using process.env.NUXT_API_KEY
+      public: {
+         // baseURL: '' // Exposed to the frontend as well.
+      }
+  },
+  css: [
+  	'~/assets/css/global.css'
+  ],
+  fonts: {
+  	families: []
+  }
 })
