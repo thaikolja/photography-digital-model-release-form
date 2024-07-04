6fa04b843c4490e744d258c98e699ca366a7d893
Kolja Nolte <kolja.nolte@gmail.com>
2024-07-05 05:09:45 +0700

    First commit with basic file templates

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  compatibilityDate: "2024-07-05",
+  modules: ["@nuxt/fonts", "@nuxtjs/tailwindcss", "@vueuse/nuxt", "@nuxtjs/i18n"]
+})
